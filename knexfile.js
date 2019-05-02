546110e6e82f68e93bde5ba095cd133a5c9e7f1e
ntsum <natsukajiwaram@gmail.com>
2019-05-02 13:32:54 -0400

    guided:

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,15 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/lambda.db3', // the folder will be created when we run the migrations
+      filename: "./data/lambda.db3" // the folder will be created when we run the migrations
     },
-  },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
